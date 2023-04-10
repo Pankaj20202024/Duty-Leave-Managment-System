c3d3eabe6dc50d80e4bfe661cb2c2b47ede0b944
Manish4359 <manishkr.official4359@gmail.com>
2023-04-10 23:55:22 +0530

    created login activity

--- app/src/main/java/com/example/dutyleavemanagmentsystem/MainActivity.kt
+++ app/src/main/java/com/example/dutyleavemanagmentsystem/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.dutyleavemanagmentsystem
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var fragment:Fragment
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //commit
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home ->
+                    // do something here
+                    true
+                R.id.events ->
+                    // do something here
+                    true
+                R.id.history ->
+                    // do something here
+                    true
+                else -> true
+            }
+        }
     }
 }
