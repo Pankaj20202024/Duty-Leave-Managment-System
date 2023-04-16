fed6761bb521b83ea4a193dc7d05465d7662edf1
aakashaman <aakashaman9931@gmail.com>
2023-04-16 14:08:12 +0530

    bottom nav

--- app/src/main/java/com/example/dutyleavemanagmentsystem/MainActivity.kt
+++ app/src/main/java/com/example/dutyleavemanagmentsystem/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.dutyleavemanagmentsystem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var homeContent: FrameLayout
+    private lateinit var eventsContent: FrameLayout
+    private lateinit var historyContent: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        homeContent = findViewById(R.id.home_content)
+        eventsContent = findViewById(R.id.events_content)
+        historyContent = findViewById(R.id.history_content)
+
+        // Set listener for when the user selects a tab
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_home -> {
+                    // Show Home content and hide others
+                    homeContent.visibility = View.VISIBLE
+                    eventsContent.visibility = View.GONE
+                    historyContent.visibility = View.GONE
+                    true
+                }
+                R.id.menu_events -> {
+                    // Show Events content and hide others
+                    homeContent.visibility = View.GONE
+                    eventsContent.visibility = View.VISIBLE
+                    historyContent.visibility = View.GONE
+                    true
+                }
+                R.id.menu_history -> {
+                    // Show History content and hide others
+                    homeContent.visibility = View.GONE
+                    eventsContent.visibility = View.GONE
+                    historyContent.visibility = View.VISIBLE
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
