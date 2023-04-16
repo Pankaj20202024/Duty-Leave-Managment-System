0e6f6e791fa8904e5bccc31c690e17c0e4ea285c
aakashaman <aakashaman9931@gmail.com>
2023-04-16 14:12:03 +0530

    bottom nav

---
+++ app/src/main/java/com/example/dutyleavemanagmentsystem/LoginActivity.kt
@@ -0,0 +1,45 @@
+package com.example.dutyleavemanagmentsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.content.Intent
+
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Initialize views
+        usernameEditText = findViewById(R.id.et_email)
+        passwordEditText = findViewById(R.id.et_password)
+        loginButton = findViewById(R.id.btn_login)
+
+        // Set click listener for login button
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Check if username and password are correct
+            if (username == "admin" && password == "password") {
+                // If correct, start MainActivity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finish LoginActivity to prevent user from going back to it
+            } else {
+                // If incorrect, show error message
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
